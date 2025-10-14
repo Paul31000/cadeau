ffead09f58bd0926e7523bb7be954293045afa99
paul688 <paul.gilbert@outlook.fr>
2025-10-14 15:42:22 +0200

    mail parametres sur serveur de prod

--- src/Controller/ResetPasswordController.php
+++ src/Controller/ResetPasswordController.php
@@ -157,7 +157,7 @@ class ResetPasswordController extends AbstractController
         }
 
         $email = (new TemplatedEmail())
-            ->from(new Address('achanger@gmail.com', 'Votre atelier noël'))
+            ->from(new Address('noreply@paul-s.fr', 'Votre atelier noel'))
             ->to((string) $user->getEmail())
             ->subject('Your password reset request')
             ->htmlTemplate('reset_password/email.html.twig')
